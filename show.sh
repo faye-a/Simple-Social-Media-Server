2e46e1a8f8ad10c16121e0e28e17618d6a0c695e
Faye Arejola <faye.arejola@ucdconnect.ie>
2020-12-01 22:49:10 +0000

    Implementing show.sh

--- show.sh
+++ show.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+#script to show the wall of user
+
+user=$1
+
+if [ $# -ne 1 ]; then
+	echo "Error: parameter problem." >&2
+	exit 1
+elif ! [ -d ~/comp30640project/project-faye-a/users/"$user" ]; then
+	echo "Error: $user does not exist." >&2
+	exit 2
+else
+echo "WallStart"
+cat ~/comp30640project/project-faye-a/users/"$user"/wall.txt
+echo "WallEnd"
+exit 0
+fi
