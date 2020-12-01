089e89a1a0de163926d29be845fa73b05e912dbd
Faye Arejola <faye.arejola@ucdconnect.ie>
2020-12-01 22:46:31 +0000

    Implementing create.sh

--- create.sh
+++ create.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+# script to create a new user
+
+user=$1
+if [ $# -ne 1 ]; then
+	echo "Error: parameters problem." >&2
+exit 1
+elif [ -d ~/comp30640project/project-faye-a/users/"$user" ]; then
+	echo "Error: user already exists." >&2
+exit 2
+else
+	cd ~/comp30640project/project-faye-a/users
+	mkdir "$user"
+	cd ~/comp30640project/project-faye-a/users/"$user"
+	mkdir friends
+	touch wall.txt
+	echo "OK: Directory created."
+exit 0
+fi
