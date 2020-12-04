6e0c2d15997782cf747b374a62fd1a5509bf3855
Faye Arejola <faye.arejola@ucdconnect.ie>
2020-12-04 13:57:05 +0000

    implementing add.sh v2

--- add.sh
+++ add.sh
@@ -7,18 +7,17 @@ friend=$2
 if [ $# -ne 2 ]; then
 	echo "Error: parameters problem" >&2
 	exit 1
-elif ! [ -d ~/comp30640project/project-faye-a/users/"$user" ]; then
+elif ! [ -d "$user" ]; then
 	echo "Error: $user does not exist." >&2
 	exit 2
-elif ! [ -d ~/comp30640project/project-faye-a/users/"$friend" ]; then
+elif ! [ -d "$friend" ]; then
 	echo "Error: $friend does not exist." >&2
 	exit 3
-elif [ -d ~/comp30640project/project-faye-a/users/"$user"/friends/"$friend" ]; then
+elif [ -d "$user/$friend" ]; then
 	echo "Error: $friend and $user are already friends." >&2
 	exit 4
 else
-cd ~/comp30640project/project-faye-a/users
-cp -r "$friend" ~/comp30640project/project-faye-a/users/"$user"/friends
+cp -r "$friend" "$user/friends"
 echo "OK: friend added."
 exit 0
 fi
