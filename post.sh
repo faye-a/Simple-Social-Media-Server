f7edc7b2fe8b700eb3508e42b7a62a639ffa71f9
Faye Arejola <faye.arejola@ucdconnect.ie>
2020-12-04 15:19:29 +0000

    implementing post.sh v2

--- post.sh
+++ post.sh
@@ -8,18 +8,17 @@ message=$3
 if [ $# -ne 3 ]; then
 	echo "Error: parameters problem." >&2
 	exit 1
-elif ! [ -d ~/comp30640project/project-faye-a/users/"$receiver" ]; then
+elif ! [ -d "users/$receiver" ]; then
 	echo "Error: Receiver $receiver does not exist." >&2
 	exit 2
-elif ! [ -d ~/comp30640project/project-faye-a/users/"$sender" ]; then
+elif ! [ -d "users/$sender" ]; then
 	echo "Error: Sender $sender does not exist." >&2
 	exit 3
-elif ! [ -d ~/comp30640project/project-faye-a/users/"$receiver"/friends/"$sender" ]; then
+elif ! grep -Fxq "$sender" "$receiver/friends"; then
 	echo "Error: Sender $sender is not friends with $receiver" >&2
 	exit 4
 else
-cd ~/comp30640project/project-faye-a/users/"$receiver"
-echo "$sender : $message" >> ~/comp30640project/project-faye-a/users/"$receiver"/wall.txt
+echo "$sender : $message" >> "$receiver"/wall
 echo "OK: message received."
 exit 0
 fi
