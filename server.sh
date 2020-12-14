06da2485937d937c9638d21bdff044f16ae5ad3f
Faye Arejola <faye.arejola@ucdconnect.ie>
2020-12-14 17:11:14 +0000

    implmenting server.sh

--- server.sh
+++ server.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+#creating a script server.sh
+
+while true; do
+	read -a arr;
+	request=${arr[0]};
+
+	case "$request" in
+		create)
+		./create.sh "${arr[1]}"
+		;;
+		add)
+		# do something
+		./add.sh "${arr[@]:1}"
+		;;
+		post)
+		# do something
+		./post.sh "${arr[@]:1}"
+		;;
+		show)
+		# do something
+		./show.sh "${arr[@]:1}"
+		;;
+		shutdown)
+		# do something
+		break
+		exit 0
+		;;
+		*)
+		echo "Error: bad request"
+		exit 1
+		;;
+	esac
+done
