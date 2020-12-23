aaa2354cc286c9712cb048041ec632bbab982038
Faye Arejola <faye.arejola@ucdconnect.ie>
2020-12-23 17:10:47 +0000

    removed array from client.sh

--- client.sh
+++ client.sh
@@ -5,35 +5,34 @@ req="$2"
 user="$3"
 friend="$4"
 message="$5"
-array=("create" "add" "post" "show" "shutdown")
 
 #error checking
 if [ "$#" -lt 2 ]; then
 	echo "Error: parameters problem." >&2
-	rm "$id".pipe
 	exit 1
-elif [[ ! "${array[$req]}" ]]; then
-	echo "Error. $req Command not recognised." >&2
-	rm "$id".pipe
-	exit 2
 
 elif [[ -p "$id".pipe ]]; then
 	echo "Error. $id.pipe already exists. Could not execute the command." >&2
 	exit 3
-else
-	mkfifo "$id".pipe
 
-	if [[ "$req" == "create" ]]; then
-		echo "$id $req $user" > server.pipe
-	elif [[ "$req" == "add" ]]; then
-		echo "$id $req $user $friend" > server.pipe
-	elif [[ "$req" == "post" ]]; then
-		echo "$id $req $user $friend  $message" > server.pipe
-	elif [[ "$req" == "show" ]]; then
-		echo "$id $req $user" > server.pipe
-	elif [[ "$req" == "shutdown" ]]; then
-		echo "$id $req" > server.pipe
-	fi
+elif [[ "$req" == "create" ]]; then
+	mkfifo "$id".pipe
+	echo "$id $req $user" > server.pipe
+elif [[ "$req" == "add" ]]; then
+	 mkfifo "$id".pipe
+	echo "$id $req $user $friend" > server.pipe
+elif [[ "$req" == "post" ]]; then
+	 mkfifo "$id".pipe
+	echo "$id $req $user $friend $message" > server.pipe
+elif [[ "$req" == "show" ]]; then
+	 mkfifo "$id".pipe
+	echo "$id $req $user" > server.pipe
+elif [[ "$req" == "shutdown" ]]; then
+	 mkfifo "$id".pipe
+	echo "$id $req" > server.pipe
+else
+	echo "Error. $req not recognised." >&2
+	exit 4
 fi
 
 #reading output of serverpipe...
@@ -43,5 +42,3 @@ while read output; do
 done < "$id".pipe
 rm "$id".pipe
 exit 0
-
-
